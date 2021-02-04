809e25f254b3bbafe12b018d1438f96575166690
Chris <chris_oleary@hotmail.co.uk>
2021-02-04 15:08:17 +0000

    Add project files.

---
+++ WpfCoreConcepts/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using WpfCoreConcepts.ViewModels;
+
+namespace WpfCoreConcepts
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, 
+                delegate 
+                {
+                    int newValue = 0;
+
+                    if (Counter == int.MaxValue)
+                    {
+                        newValue = 0;
+                    }
+                    else
+                    {
+                        newValue = Counter + 1;
+                    }
+                    SetValue(CounterProperty, newValue);
+                }, Dispatcher);
+        }
+
+        public int Counter
+        {
+            get { return (int)GetValue(CounterProperty); }
+            set { SetValue(CounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Counter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CounterProperty =
+            DependencyProperty.Register("Counter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+    }
+}
---
+++ WpfCoreConcepts/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfCoreConcepts.ViewModels
+{
+    public class MainViewModel : DependencyObject
+    {
+
+
+
+
+      
+
+
+
+
+
+    }
+}
